93de297a0add7e1f5e14dd6b6fb4fc80e2714337
Hermann EHO <hermannleboss@gmail.com>
2023-02-08 19:15:07 +0000

    feat: creat exception on home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,11 @@
+<script setup>
+  const checkout =  ()=> {
+    throw new Error("checkout is not yet implemented")
+  }
+</script>
 <template>
   <div>
     <h1 class="text-4xl">Home</h1>
+    <button @click="checkout">Checkout</button>
   </div>
 </template>
