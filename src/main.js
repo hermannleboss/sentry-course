4eb60e025e9d4bbf74444b42a270b2ed880ad46e
Hermann EHO <hermannleboss@gmail.com>
2023-02-08 19:15:32 +0000

    feat: enable error display in browser console

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ const app = createApp(App)
 Sentry.init({
     app,
     dsn: "https://8d447f1ea57e45bd8dd251991bf9707b@o4504645703172096.ingest.sentry.io/4504645717590016",
+    logErrors: true,
     integrations: [
         new BrowserTracing({
             routingInstrumentation: Sentry.vueRouterInstrumentation(router),
