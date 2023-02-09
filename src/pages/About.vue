5cf797c4e365fd710100b6ca53dda12ae5f83532
Hermann EHO <hermannleboss@gmail.com>
2023-02-09 15:44:09 +0000

    chore: new error for test

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
-    <button @click="newFunction()">Sen Email</button>
+    <button @click="helloWorld()">Sen Email</button>
   </div>
 </template>
 
 <script setup>
+const helloWorld = () =>{
+  console.log("call of hello world")
+  throw new Error('Une nouvelle erreur')
+}
 </script>
